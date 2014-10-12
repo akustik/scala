96cb06e7c9d3ea7472c74e745ad9e45d5be2fddf
Guillermo Mercadal <guillermo.mercadal@king.com>
2014-10-12 21:01:57 +0200

    Use imported project

--- sbt/serious/src/main/scala/app/Launcher.scala
+++ sbt/serious/src/main/scala/app/Launcher.scala
@@ -1,12 +1,16 @@
 package app
 
 import app.core._
+import org.gmd.shell._
 
 /**
  * Created by guillermo.mercadal on 28/09/14.
  */
 object Launcher {
   def main(args: Array[String]) {
+    val shellExecutor = new Executor
+    println(shellExecutor("ls -l"))
+
     val consumer = new AppConsumer
     args.foreach(consumer.apply)
   }
